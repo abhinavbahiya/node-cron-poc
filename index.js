bf488d0c2a4b43ef5772f724fb6594226a6e6a38
Abhinav Bahiya <Abhinav.Bahiya@asurion.com>
2019-04-22 20:27:09 +0530

    - Adds moment.js - Adds AWS-SDK

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = process.env.PORT || 2001;
 const cron = require('node-cron');
 const nodemailer = require('nodemailer');
+const moment = require('moment');
+const AWS = require('aws-sdk');
 
 const myFirstMiddleware = (req, res, next) => {
   console.log('Hi I am in middleware');
-  console.log(process.env)
   next();
 }
 
@@ -15,28 +16,60 @@ const myFirstController = (req, res) => {
   res.status(200).json('Hi there!');
 }
 
+const getEmails = (time) => {
+  let emailIds = [];
+  AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+  });
+
+  let docClient = new AWS.DynamoDB.DocumentClient();
+
+  var TableName = "Information"; // Name of the table
+
+  var params = {
+    TableName, // Use of shorthand
+    Key: {
+      "time": time
+    }
+  };
+
+  docClient.get(params, function (err, data) {
+    if (err) {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+      emailIds = data;
+    }
+  });
+  return emailIds;
+}
+
 const myCronController = async (req, res) => {
   try {
-    let account = await nodemailer.createTestAccount();
-
     let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
+      host: 'gmail',
       port: 465,
       secure: true,
       auth: {
-        user: process.env.EMAIL || account.user,
-        pass: process.env.PASSWORD || account.pass
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD // Environment Variable's aren't pushed to source control
       }
     });
+    let time = moment.normalizeUnits('UTC').format().split('T')[1].split(':');
+    time = time[0].toString() + time[1].toString();
+    let emailIds = getEmails(time);
+
+    let emailString = emailIds.join(', '); // Comma & space
 
     let mailoptions = {
-      from: '"Fred Foo 👻" <foo@example.com>',
-      to: 'thepotatofever@example.com',
+      from: 'abhinavbahiya@gmail.com',
+      to: emailString,
       subject: 'My first email',
-      text: 'Congratulations! The Potato Fever'
+      text: `Congratulations! You've been emailed at 8 am of your local time.`
     }
 
-    cron.schedule('* * * * * *', async () => {
+    cron.schedule('*/30 * * * *', async () => { // Runs cron job in every 30 minutes
       console.log("Running something");
       let info = await transporter.sendMail(mailoptions, (error, info) => {
         if (error) {
